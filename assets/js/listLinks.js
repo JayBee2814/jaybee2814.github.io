e277da8bf1a86237081d4ab6ce28753c16a8147d
JoshBayne <josh.bayne222@gmail.com>
2020-10-16 13:53:25 -0500

    Added Hats

--- assets/js/listLinks.js
+++ assets/js/listLinks.js
@@ -20,3 +20,13 @@ $('a.cameras').click(function(e) {
     window.open('https://www.ebay.com/sch/i.html?_from=R40&_trksid=p2334524.m570.l1313&_nkw=sony+a6100&_sacat=0&LH_TitleDesc=0&_osacat=0&_odkw=sony+a6000');
     window.open('https://www.ebay.com/sch/i.html?_from=R40&_trksid=p2334524.m570.l1311&_nkw=fujifilm+x-t30&_sacat=0&LH_TitleDesc=0&_osacat=0&_odkw=sony+a6100');
 });
+
+// These are the links for the hats
+$('a.hats').click(function(e) {
+    e.preventDefault();
+    window.open('https://www.ebbets.com/collections/ballcaps/products/tokyo-kyojin-giants-1940-ballcap');
+    window.open('https://www.ebbets.com/collections/ballcaps/products/hiroshima-carp-1958-ballcap');
+    window.open('https://www.ebbets.com/collections/ballcaps/products/bismarck-churchills-1935-ballcap');
+    window.open('https://www.ebbets.com/collections/ballcaps/products/brooklyn-bushwicks-1949-ballcap');
+    window.open('https://www.ebbets.com/collections/ballcaps/products/salt-lake-bees-1925-vintage-ballcap');
+});
